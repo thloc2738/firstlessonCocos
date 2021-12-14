b205d19fac58bd87a4b8c2b17e17ccc0d289675d
Tran Huu Loc <thloc597@gmail.com>
2021-12-15 00:57:10 +0700

    push script

--- Final_test_2048/assets/Script/add.js
+++ Final_test_2048/assets/Script/add.js
@@ -11,66 +11,33 @@ cc.Class({
     onLoad() {
         this.arr.push(2);
         this.arr.push(2);
-        this.arr.push(2);
+        this.arr.push(0);
         this.arr.push(2);
         cc.log(this.arr);
         let leng = this.arr.length - 1;
         for (let i = this.arr.length - 1; i >= 0; i--) {
             let k = i;
             let z = i;
-            if (k > 0) {
-                while (this.arr[k] == 0 && k > 0) {
-                    if (this.arr[k] + this.arr[k - 1] == this.arr[k - 1]) {
-                        this.arr[k] += this.arr[k - 1];
-                        this.arr[k - 1] = 0;
-                        k--;
-                    }
-
+            if (i > 0) {
+          
+                cc.log("123" + this.arr);
+                while((this.arr[k] + this.arr[k-1] == this.arr[k -1] || this.arr[k] - this.arr[k-1] == 0) && k > 0){
+                    this.arr[k] += this.arr[k-1];
+                    this.arr[k-1] = 0;
+                    k--;
                 }
-                while (this.arr[z] != 0 && z > 0) {
-                    if (this.arr[z] == this.arr[z - 1]) {
-                        this.arr[z] += this.arr[z - 1];
-                        this.arr[z - 1] = 0;
-                        z--
-                    }
-                }
-
             }
             else {
-                let j = 0;
-                // while (this.arr[leng - k] == 0 && k < leng) {
-                //     if (this.arr[leng - k] + this.arr[leng - k - 1] == this.arr[leng - k - 1]) {
-                //         this.arr[leng - k] = this.arr[leng - k - 1]
-                //         this.arr[leng - k - 1] = 0;
-                //         k++;
-                //     }
+               if(this.arr[this.arr.length - 1] == 0){
+                   if(this.arr[this.arr.length - 1 - i] == 0 && i < this.arr.length){
+                       i++;
+                   }
+                   if(this.arr[this.arr.length - 1 - i] != 0){
+                    this.arr[this.arr.length - 1] += this.arr[this.arr.length - 1 - i];
+                    this.arr[this.arr.length - 1 - i] = 0;
+                   }
 
-                // }
-                k = 0;
-
-                while (this.arr[leng - j] == this.arr[leng - j - 1] && j < leng) {
-                    if (this.arr[leng - j] + this.arr[leng - j - 1] == this.arr[leng - j] * 2) {
-                        this.arr[leng - j] += this.arr[leng - j - 1];
-                        this.arr[leng - j - 1] = 0;
-                        cc.log(this.arr[leng - j - 1]);
-                        j++
-                    }
-                    if (this.arr[leng - j] + this.arr[leng - j - 1] == this.arr[leng - j - 1]) {
-                        this.arr[leng - j] = this.arr[leng - j - 1]
-                        this.arr[leng - j - 1] = 0;
-                        j++;
-                    }
-                }
-                while (this.arr[k] != 0 && k < leng - 1) {
-                    if (this.arr[k] + this.arr[k + 1] == this.arr[k]) {
-                        this.arr[k + 1] += this.arr[k];
-                        this.arr[k] = 0;
-                    }
-                    k++
-                }
-
-
-
+               }
             }
             cc.log(this.arr)
 
