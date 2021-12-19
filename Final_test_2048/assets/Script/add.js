1c544ce6e4165dca6e81d8e8d743df70f8597539
Tran Huu Loc <thloc597@gmail.com>
2021-12-19 10:22:39 +0700

    update

--- Final_test_2048/assets/Script/add.js
+++ Final_test_2048/assets/Script/add.js
@@ -9,17 +9,11 @@ cc.Class({
     //4 0 4 4 => 0 0 4 8
 
     onLoad() {
-        // this.arr.push(2);
-        // this.arr.push(2);
-        // this.arr.push(4);
-        // this.arr.push(2);
-
-        // cc.log(this.arr)
-        // this.goRight(this.arr);
-       
+     
     },
 
     goRight(array) {
+        cc.log(array)
         for (let i = array.length - 1; i >= 0; i--) {
             let k = i;
             if (i > 0) {
@@ -34,16 +28,18 @@ cc.Class({
                     }
                 }
             }
-
         }
-
+        cc.log(array)
         for (let i = array.length - 1; i >= 0; i--) {
             let k = i;
             if (i > 0) {
-               while(array[i] == array[k-1] && i > 0){
-                   array[i] += array[k-1];
-                   array[k-1] = 0;
-               }
+                if(array[i]!=0){
+                    while(array[i] == array[k-1] && i > 0){
+                        array[i] += array[k-1];
+                        array[k-1] = 0;
+                    }
+                }
+               
             }
         }
         cc.log(array)
@@ -64,73 +60,7 @@ cc.Class({
         }
         cc.log(array)
     },
-    goLeft(){
-         for (let i = 0; i < this.arr.length - 1; i++) {
-            let j = i;
-            while (this.arr[i] == 8 && i < this.arr.length) {
-                if (this.arr[j + 1] == 8) {
-                    j++;
-                }
-                else {
-                    this.arr[i] = this.arr[j + 1];
-                    this.arr.length--;
-
-                }
-            }
-        }
-        cc.log(this.arr);
-        cc.log(this.arr.length)
-        cc.log(this.arr);
-        for (let i = 0; i < this.arr.length; i++) {
-            let k = i;
-            if (i < this.arr.length -1) {
-                    while(this.arr[i] == 0 && (k % 4) < 3){
-                        if(this.arr[k+1] == 0){
-                            k++;
-                        }
-                        else{
-                            this.arr[i] += this.arr[k+1];
-                            this.arr[k+1] = 0;
-                            k++;
-                        }
-                    } 
-            }
-        }
-        cc.log(this.arr);
-        for(let i = 0; i < this.arr.length; i++) {
-            let k = i;
-            if(i < this.arr.length -1){
-                while(this.arr[i] !=0 &&(k % 4) < 3){
-                    if(this.arr[i] != this.arr[k+1]){
-                        k++
-                    }
-                    else{
-                        this.arr[i] += this.arr[k+1];
-                        this.arr[k+1] = 0;
-                        k++
-                    }
-                }
-            }
-        }
-        cc.log(this.arr);
-        for (let i = this.arr.length -1; i >= 0; i--) {
-            let k = i;
-            if (i > 0) {
-                    while(this.arr[i] == 0 && (k%4) > 0){
-                        if(this.arr[k-1] == 0){
-                            k--;
-                        }
-                        else{
-                            this.arr[i] = this.arr[k-1];
-                            this.arr[k-1] = 0;
-                            k--;
-                        }
-                    }
-
-            }
-        }
-        cc.log(this.arr)
-    },
+   
     start() {
 
     },
--- Final_test_2048/assets/Script/controller.js
+++ Final_test_2048/assets/Script/controller.js
@@ -6,12 +6,13 @@ cc.Class({
     properties: {
         mainScene: cc.Node,
         item: cc.Prefab,
+        emptyPrefab: cc.Prefab,
         listFlag: [],
         boxLayout: cc.Layout,
         xyPosition: [],
         fillTable: [],
         isFill: [],
-        count: 0,
+        _count: 0,
         arr: [],
     },
     createTable() {
@@ -34,109 +35,123 @@ cc.Class({
                 y = 27 + ((i - 3) / 4) * (-155);
                 pos = cc.v3(x, y, 0);
             }
-            cc.log(pos)
+            // cc.log(pos)
             this.xyPosition.push(pos);
         }
 
         cc.log(this.xyPosition)
     },
-    createItem(value, index) {
-        let _item = cc.instantiate(this.item);
-        this.mainScene.addChild(_item);
-        this.mainScene.children[index].getChildByName("numb").getComponent(cc.Label).string = 2;
-        this.mainScene.children[index].position = this.xyPosition[value];
-        cc.tween(this.mainScene.children[index])
+    onLoad() {
+        this.createPrefabsTable(this.mainScene, this.emptyPrefab)
+        this.emptyTable(this.isFill, 0);
+        this.createTable()
+        // cc.log(this.fillTable[0].childrenCount)
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.moveRight, this);
+        this.mainScene.on(cc.Node.EventType.MOUSE_DOWN, function (event) {
+            this.addItemInBox(this.isFill);
+            // this.goRight(this.isFill);
+        }, this);
+        
+    },
+    createItem(value) {
+        cc.log("Object: " + this.mainScene)
+        this.mainScene.children[value].addChild(cc.instantiate(this.item));
+        this.mainScene.children[value].children[1].getChildByName("numb").getComponent(cc.Label).string = 2;
+        this.mainScene.children[value].position = this.xyPosition[value];
+        this.isFill[value] = 2
+        cc.tween(this.mainScene.children[value].children[1])
             .to(0.5, { scale: 1 })
             .start()
-        this.count++;
-        return this.mainScene.children[index];
+        this._count++;
+        this.fillTable[value] = this.mainScene.children[value];
+        cc.log(this.fillTable[value].childrenCount)
+       
+        return this.mainScene.children[value];
     },
-    isCreate(node, list) {
-        for (let index = 0; index <= list.length; index++) {
-            if (node.position == list[index]) {
+
+    
+    
+    addItemInBox(listItem) {
+        if(this.checkAddItem()){
+            let number;
+            // cc.log(this.fillTable.length)
+            // cc.log(this.fillTable.length + " length")
+            do {
+                number = Math.floor(Math.random() * 16);
+                cc.log("Number: "+number);
+            } while (listItem[number] != 0);
+            
+            this.createItem(number);
+            // this.fillTable[number] = this.createItem(number);
+            // cc.log(this.fillTable);
+            listItem[number] = 2;
+            
+            // cc.log("1 "+listItem.length);
+            // cc.log(this.fillTable[number].children[1].getChildByName("numb").getComponent(cc.Label).string)
+        }
+    },
+
+    checkAddItem(){
+        for(let i = 0; i< this.isFill.length; i++){
+            if(this.isFill[i] == 0){
                 return true;
             }
         }
-        return false;
-    },
-    onLoad() {
-        this.emptyTable(this.isFill, 0);
-        this.createTable()
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.moveRight, this);
-        this.mainScene.on(cc.Node.EventType.MOUSE_DOWN, function (event) {
-            cc.log(this.isFill);
-            this.addItemInBox(this.isFill);
-        }, this);
-    },
-    addItemInBox(listItem) {
-        let number;
-        if (this.fillTable.length <= 15) {
-            do {
-                number = Math.floor(Math.random() * 16);
-
-            } while (this.isCreate(number, this.fillTable) == true && this.fillTable.length <= 15 && listItem[number] != 0);
-
-            this.createItem(number, this.count);
-            this.fillTable.push(this.createItem(number, this.count));
-            cc.log(this.fillTable)
-            listItem[number] = 2;
-            cc.log(listItem);
-            cc.log(this.fillTable)
-        }
+        return false
     },
 
     moveRight: function (event) {
         switch (event.keyCode) {
             case cc.macro.KEY.right:
-                cc.log("right");
-                this.right_table();
-                this.goRight(this.isFill);
+                cc.log(this.isFill)
+                // this.right_table();
+                this.goRight(this.fillTable);
                 break;
         }
     },
     right_table() {
-        if (this.fillTable.length > 0) {
-            for (let j = 0; j < this.fillTable.length; j++) {
-
-
-            }
-        }
+        
         cc.log(this.fillTable);
     },
     goRight(array) {
+        
         for (let i = array.length - 1; i >= 0; i--) {
             let k = i;
             if (i > 0) {
-                while (array[i] == 0 && (k % 4) > 0) {
-                    if (array[k - 1] == 0) {
+                while (array[i].childrenCount != 2 && (k % 4) > 0) {
+                    cc.log("Array[i] " + array[i])
+                    if (array[k - 1].childrenCount != 2) {
+                        cc.log("321" + array[i])
                         k--;
                     }
                     else {
-                        array[i] += array[k - 1];
-                        array[k - 1] = 0;
+                        cc.log(array[i]+ "123")
+                        // array[i].children[2].getChildByName("numb").getComponent(cc.Label).string += array[k - 1].children[2].getChildByName("numb").getComponent(cc.Label).string;
+                        // array[k - 1].children[2].destroy();
                         k--;
                     }
                 }
             }
-
         }
         // cc.log(array)
-
         for (let i = array.length - 1; i >= 0; i--) {
             let k = i;
             if (i > 0) {
-               while(array[i] == array[k-1] && i > 0){
-                   array[i] += array[k-1];
-                   array[k-1] = 0;
-               }
+                if(array[i]!=0){
+                    while(array[i] == array[k-1] && i > 0){
+                        array[i] += array[k-1];
+                        array[k-1] = 0;
+                    }
+                }
+               
             }
         }
         // cc.log(array)
         for (let i = array.length - 1; i >= 0; i--) {
             let k = i;
             if (i > 0) {
-                while (array[i] == 0 && (k % 4) > 0) {
-                    if (array[k - 1] == 0) {
+                while (array[i].childrenCount != 2 && (k % 4) > 0) {
+                    if (array[k - 1].childrenCount != 2) {
                         k--;
                     }
                     else {
@@ -147,7 +162,7 @@ cc.Class({
                 }
             }
         }
-        cc.log(array)
+        // cc.log(array)
     },
     goLeft() {
         for (let i = array.length - 1; i >= 0; i--) {
@@ -221,4 +236,13 @@ cc.Class({
         }
 
     },
+    createPrefabsTable(parentNode ,prefab){
+        for(let index = 0; index < 16; index++){
+            let item = cc.instantiate(prefab);
+            parentNode.addChild(item);
+            parentNode.children[index].addChild(cc.instantiate(this.emptyPrefab))
+            this.fillTable[index] = parentNode.children;
+        }
+        cc.log(parentNode.children[0].childrenCount)
+    }
 });
